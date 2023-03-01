51ef40d67ac0ba1aca4acf53bfac059ce3f37304
beat-h <beat.hulliger@fhnw.ch>
2023-03-01 17:08:44 +0100

    changed test for plotMD to expect_type

--- tests/testthat/test_plotIT.R
+++ tests/testthat/test_plotIT.R
@@ -4,5 +4,6 @@ library(modi)
 test_that("Plot of infection times of Epidemic Algorithm", {
   it <- c(rep(NA, 3), rep(1:7, times=c(1, 4, 10, 8, 5, 3, 2)))
   wt <- rep(c(1,2,5), times=12)
-  plotIT(it, wt, 6)
+  pout <- plotIT(it, wt, 6)
+  expect_null(pout)
 })
--- tests/testthat/test_plotMD.R
+++ tests/testthat/test_plotMD.R
@@ -4,5 +4,6 @@ library(modi)
 test_that("Plot of Mahalanobis distance", {
   data(bushfirem, bushfire.weights)
   det.res <- TRC(bushfirem, weights = bushfire.weights)
-  PlotMD(det.res$dist, ncol(bushfirem))
+  pout <- PlotMD(det.res$dist, ncol(bushfirem))
+  expect_type(pout, type = "list")
 })
